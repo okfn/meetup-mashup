b278c3981dcd58fe019046997a07e21ddfcfb911
Tom Rees <zephod@gmail.com>
2012-10-16 21:13:15 +0100

    Code updated

--- README.markdown
+++ README.markdown
@@ -2,4 +2,7 @@ Some Python code to pull data from the Meetup.com API.
 
 Requirements: 
 
-    pip install requests
+    $ pip install requests
+    $ export MEETUP_API_KEY=abc1234...
+
+Note that your Meetup API key can be retrieved from http://www.meetup.com/meetup_api/key/.
--- get.py
+++ get.py
@@ -1,20 +1,26 @@
 import urllib
 import os
 import requests
+import json
 
-def endpoint(method, parameters):
+container_name = 'OpenKnowledgeFoundation'
+container_id = 14142
+
+def get_json(method, parameters):
     api_key = os.environ.get('MEETUP_API_KEY')
     assert api_key is not None, 'No MEETUP_API_KEY defined in environment.'
     assert method[0] is '/', 'Endpoint must be prefixed with "/" eg. "/ew/communities"'
     url_parameters = urllib.urlencode(parameters)
-    return 'https://api.meetup.com%s?key=%s&%s' % (method,api_key,url_parameters)
+    url = 'https://api.meetup.com%s?key=%s&%s' % (method,api_key,url_parameters)
+    r = requests.get(url)
+    return json.loads(r.text)
 
 def list_communities():
-    url = endpoint( '/ew/communities', {'urlname':'OpenKnowledgeFoundation'} )
-    r = request.get(url)
-    print vars(r)
-    return r.text
+    return get_json( '/ew/communities', {'container_id':container_id} )
+
+def list_events():
+    return get_json( '/ew/events', {'container_id':container_id} )
 
 if __name__=='__main__':
-    print list_communities()
+    print json.dumps(list_events(), indent=4)
 
